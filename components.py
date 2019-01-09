8c294f537f5e241249360bf94feefe56be7773bc
frosty-nee <frostbite@frosty-nee.net>
2019-01-09 17:53:07 +0000

    finished Groups and Tables listing

---
+++ components.py
@@ -0,0 +1,50 @@
+components = {
+    0:[ 'Balloon Deployer': {
+
+    }],
+    1:['Drive Maintainer': {
+
+    }],
+    2:['Air Pump': {
+
+    }],
+    3:['Resource Gatherer': {
+
+    }],
+    4:['Oil Drill': {
+
+    }],
+    5:['Ammo Processor', {
+
+    }],
+    6:['Fuel Refinery',{}],
+    7:['Tractor Beams',{}],
+    8:['Hydrofoils',{}],
+    9:['Propulsion',{}],
+    10:['Shield Projector',{}],
+    11:['Helium Pump',{}],
+    12:['Spotlight',{}],
+    13:['Advanced Cannon Laser Targeter',{}],
+    14:['CRAM Cannon Laser Targeter',{}],
+    15:['Warp Drive',{}],
+    16:['Particle Cannon Lense',{}],
+    17:['Steam Boiler Controller',{}],
+    18:['Fuel Engine Generator',{}],
+    19:['Sail Main Block',{}],
+    20:['Advanced Cannon Ammo Intake',{}],
+    21:['Advanced Cannon Ammo Controller',{}],
+    22:['AI Wireless Transmitter',{}],
+    23:['AI Wireless Reciever',{}],
+    24:['AI Aimpoint Selection Card',{}],
+    25:['Detection Component',{}],
+    26:['AI Mainframe',{}],
+    27:['Laser Colorer',{}],
+    28:['Laser Missile Defense',{}],
+    29:['Particle Cannon',{}],
+    30:['Light Fitting',{}],
+    31:['Simple Laser',{}],
+    32:['Smoke Generator',{}],
+    33:['Hologram Projector',{}],
+    34:['Poster Holder',{}],
+    35:['Electric Engine',{}],
+}
--- ftdocs.py
+++ ftdocs.py
@@ -2,54 +2,9 @@
 
 import sys
 
-groups = {
-        'Propulsion': [
-            'TellAiThatWeAreTakingControl',
-            'RequestControl',
-            'RequestThrustControl',
-            'RequestWaterForwards',
-            'RequestComplexControllerStimulus',
-            'MoveFortress',
-            ],
-        'Target Info': [
-            'GetNumberOfMainframes',
-            'GetNumberOfTargets',
-            'GetTargetInfo',
-            'GetTargetPositionInfo',
-            'GetTargetPositionInfoForPosition',
-            ],
-        'Misc': [
-            'GetTerrainAltitudeForPosition',
-            'GetTerrainAltitudeForLocalPosition',
-            'GetGravityForAltitude',
-            'GetTime',
-            'GetTimeSinceSpawn',
-            'GetGameTime',
-            ],
-        'Self Awareness': [
-            'GetConstructPosition',
-            'GetConstructForwardVector',
-            'GetConstructRightVector',
-            'GetConstructUpVector',
-            'GetConstructMaxDimensions',
-            'GetConstructMinDimensions',
-            'GetConstructRoll',
-            'GetConstructPitch',
-            'GetConstructYaw',
-            'GetConstructCenterOfMass',
-            'GetAiPosition',
-            'GetVelocityMagnitude',
-            'GetForwardsVelocityMagnitude',
-            'GetVelocityVector',
-            'GetVelocityVectorNormalized',
-            'GetAngularVelocity',
-            'GetLocalAngularVelocity',
-            'GetAmmoFraction',
-            'GetFuelFraction',
-            'GetSparesFraction',
-            ],
-
-        }
+from groups import groups
+from commands import commands
+from tables import tables
 
 if __name__ == '__main__': 
     if len(sys.argv) > 1:
@@ -58,10 +13,16 @@ if __name__ == '__main__':
             for command in groups[arg]:
                 print(command)
         elif arg in commands:
-            print(commands[arg])
-        
+            for pair in commands[arg].items():
+                print('{:20} {}'.format(*pair))
+        elif arg in tables:
+            for pair in tables[arg].items():
+                print('{:20} {}'.format(*pair))
         else: print('command or command group not found')
     else:
-        print("command groups:")
+        print("Command Groups:")
         for line in groups.keys():
+            print(line)
+        print('\nTables:')
+        for line in tables.keys():
             print(line)
